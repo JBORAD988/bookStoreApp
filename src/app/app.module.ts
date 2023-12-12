3a0d15931703da2f91bfc2c7cd72b79bc84942c5
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-12 16:49:55 +0530

    update2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { AuthComponent } from './auth/auth.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HowItWorksComponent,
+    AuthComponent,
+
   ],
   imports: [
     BrowserModule,
