3e82876f2f96dc034eaed4ccd4c950e096f5f145
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-13 18:47:48 +0530

    update_4.2

--- src/app/public/components/book-details/book-details.component.ts
+++ src/app/public/components/book-details/book-details.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss'],
 })
-export class BookDetailsComponent {}
+export class BookDetailsComponent implements OnInit{
+
+  public id: number =0;
+  public authorId: number = 0;
+  constructor(private route: ActivatedRoute) {}
+  //
+  // ngOnInit() {
+  //   console.log(this.route);
+  //   this.route.params.subscribe((Param ):void=>{
+  //     // console.log(param);
+  //     this.id= Param.bookId;
+  //     this.authorId = Param.authorId;
+  //   });
+  //
+  // }
+
+
+  ngOnInit() {
+    console.log(this.route);
+    this.route.params.subscribe((params): void => {
+      console.log(params)
+      // Use correct parameter names (params instead of Param)
+      // Also, use '+' to convert string to number if needed
+      this.id = params['id'];
+      this.authorId = params['authorId'];
+    });
+  }
+
+
+}
