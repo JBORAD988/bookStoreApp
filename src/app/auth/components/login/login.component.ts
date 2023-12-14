734991a914e7c77107f42edaf4d124d8cef676a7
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-14 18:21:00 +0530

    update_4.3.2

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,30 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Route, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+  name: string = '';
+  email: string = '';
+  id: string = '';
 
-  name:string ='';
-  email:string='';
-  id:string='';
-
-  constructor(private route: ActivatedRoute ) {
-  }
+  constructor(private route: ActivatedRoute, private clickroute: Router) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(queryParam =>{
-      this.name=queryParam['name'],
-        this.email=queryParam['email'],
-        this.id=queryParam['id']
+    this.route.queryParams.subscribe((queryParam) => {
+      (this.name = queryParam['name']),
+        (this.email = queryParam['email']),
+        (this.id = queryParam['id']);
 
-      console.log(queryParam)
-    })
-
-
+      console.log(queryParam);
+    });
   }
 
+  goToSignup():void{
+    this.clickroute.navigate(['auth/signup'])
+  }
+
+  // goToBookDetails():void{
+  //   // this.clickroute.navigate(['public/book-details',100,'author',300])
+  //   this.clickroute.navigate(['public/book-details',100,'author',this.id])
+  // }
+  goToBookDetails(id:number, authorId: number):void{
+    // this.clickroute.navigate(['public/book-details',100,'author',300])
+    this.clickroute.navigate(['public/book-details',id,'author',authorId],{queryParams: {name:'Jay',email:'jay@gmail.com'}})
+  }
 }
