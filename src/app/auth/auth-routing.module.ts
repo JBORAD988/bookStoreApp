ea659a5a685566fd00f0d5c9969ebe95c0a927c4
jayborad <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-15 12:42:42 +0530

    update_4.4.1

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -6,18 +6,23 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import {ChangePasswordComponent} from "./components/change-password/change-password.component";
 
+// const routes: Routes = [
+//   {
+//     path: 'auth',
+//     component: AuthComponent,
+//     children: [
+//       { path: 'login', component: LoginComponent },
+//       { path: 'signup', component: SignupComponent },
+//       {path:'change-password', component: ChangePasswordComponent}
+//     ],
+//   },
+// ];
+
 const routes: Routes = [
-  {
-    path: 'auth',
-    component: AuthComponent,
-    children: [
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
       {path:'change-password', component: ChangePasswordComponent}
-    ],
-  },
 ];
-
 @NgModule({
   declarations: [],
   imports: [RouterModule.forChild(routes)],
