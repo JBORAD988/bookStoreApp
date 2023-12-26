0f8f91f3fb927c68a7d0fd6e7510a3289733e999
Jay <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-26 12:18:54 +0530

    update bytortois

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { AuthComponent } from './auth/auth.component';
+import {SharedModule} from "./shared/shared.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HowItWorksComponent,
+    AuthComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AuthModule,
+    // UserModule,
+    // PublicModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
