80a297752713808cf65da0c371cce37087442129
Jay <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-12-28 17:20:28 +0530

    update 6.1.1

--- src/app/books/components/all-books/all-books.component.ts
+++ src/app/books/components/all-books/all-books.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BookModel} from "../../models/book.model";
 import {BookService} from "../../services/book.service";
-import {CounterService} from "../../../shared/services/counter.service";
 import {Counter2Service} from "../../../shared/services/counter2.service";
 
 
@@ -13,10 +12,35 @@ import {Counter2Service} from "../../../shared/services/counter2.service";
 })
 export class AllBooksComponent implements OnInit{
 
+
+
+  private _pageTitle: string;
+
+  public set pageTitle(value: string) {
+    this._pageTitle = value;
+  }
+
+  public get pageTitle(): string {
+    return this._pageTitle;
+  }
+
   public books: BookModel[] = [];
   constructor(public bookService: BookService, public _CounterService:Counter2Service) {}
   ngOnInit() {
-    this.books = this.bookService.getBooks();
+
+    this.pageTitle = 'All Books';
+    const allBooks = this.bookService.getBooks();
+    allBooks.forEach(b => {
+      let obj = new BookModel();
+      obj.id= b.id;
+      obj.author = b.author;
+      obj.price = b.price;
+      obj.title = b.title;
+      obj.totalPages = b.totalPages;
+      this.books.push(obj);
+    })
+
+    // this.books = this.bookService.getBooks();
     console.log(this.books)
 
   }
