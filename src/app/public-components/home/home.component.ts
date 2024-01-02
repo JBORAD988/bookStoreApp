06fb74e8367bad20740a166f92f9e61196628093
Jay <co-20-081@rctiahmedabad.gujgov.edu.in>
2024-01-02 17:47:22 +0530

    update 6.4.5_ngAfterViewInit

--- src/app/public-components/home/home.component.ts
+++ src/app/public-components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {TestService} from "../../shared/services/test.service";
 import {AuthorModel} from "../../shared/models/author.model";
 
@@ -8,16 +8,23 @@ import {AuthorModel} from "../../shared/models/author.model";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
-
+export class HomeComponent implements OnInit , AfterViewInit{
+@ViewChild('btnCounter') btnCounter: ElementRef;
 
   constructor()
   {
     console.log("hello from parent constructor")
   }
 
+  ngAfterViewInit() {
+    console.log(this.btnCounter);
+    this.btnCounter.nativeElement.innerHTML = " button text Updated";
+  }
+
   ngOnInit() {
-    console.log("hello from parent ngOnInit")
+    console.log("hello from parent ngOnInit");
+
+
 
   }
 
